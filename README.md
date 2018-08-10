e839ecd4ac738e16266e0853043b6a5b1449c269
Manuel Soto Romero <manu@ciencias.unam.mx>
2018-08-10 01:52:19 -0500

    Primer commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+<p align="center">
+  <img src="http://lenguajesfc.com/20191/images/ciencias.png" align="right" hspace="5">
+  <h1>Lenguajes de Programación, 2019-1</h1>
+</p>
+
+Práctica 1: Introducción a Racket
+---------------------------------
+
+### Fecha de entrega
+
+30 de agosto de 2018
+
+### Integrantes
+
+* Nombre del alumno 1 | Correo del alumno 1
+* Nombre del alumno 2 | Correo del alumno 2
+* Nombre del alumno 3 | Correo del alumno 3
---
+++ src/practica1.rkt
@@ -0,0 +1,73 @@
+#lang plai
+
+#| Lenguajes de Programación
+   Práctica 1 |#
+
+;; Función que toma dos números enteros y los eleva a sí mismos para luego sumar las potencias, es
+;; decir, debe regresar a^b + b^a.
+;; pot-sum: number number -> number
+(define (pot-sum a b)
+    #| Aquí va su código. |#)
+
+;; Función que calcula el área de un triángulo dados sus lados, usando la fórmula de Herón.
+;; area-heron: number number number -> number
+(define (area-heron a b c)
+    #| Aquí va su código. |#)
+
+;; Función que dado un día de la semana representado mediante los 0 = do, 1 = lu, 2 = ma, ... sá = 6,
+;; y un valor booleano y un valor booleano indicando si son vacaciones, regresa un símbolo que indica
+;; a qué hora sonará una alarma.
+;; alarma: number boolean -> symbol
+(define (alarma dia vacaciones)
+    #| Aquí va su código. |#)
+
+;; Predicado que dados dos números enteros, determina si el segundo es divisor propio del primero.
+;; divisor-propio?: number number -> boolean
+(define (divisor-propio? n m)
+    #| Aquí va su código. |#)
+
+;; Predicado que dado un número natural, determina si es un número perfecto.
+;; es-perfecto?: number -> boolean
+(define (es-perfecto? n)
+    #| Aquí va su código. |#)
+
+(define (son-amigos? a b)
+    #| Aquí va su código. |#)
+
+;; Predicado que dado un número natural, determina si un número es feliz.
+;; es-feliz?: number -> boolean
+(define (es-feliz? n)
+    #| Aquí va su código. |#)
+
+;; Función recursiva que encuentra los números primos en un rango de m a n usando la Criba de 
+;; Eratóstenes.
+;; criba-eratostenes number number -> (listof number)
+(define (criba-eratostenes n)
+    #| Aquí va su código. |#)
+
+;; Función recursiva que toma un número y regresa una lista de pares con la descomposición en primos
+;; del mismo.
+;; descomposicion-primos: number -> (listof number)
+(define (descomposicion-primos n)
+    #| Aquí va su código. |#)
+
+;; Función que recibe una lista de números entre 0 y 99 y regresa una lista con su representación en
+;; japones.
+;; a-japones: (listof number) -> (listof string)
+(define (a-japones lista)
+    #| Aquí va su código. |#)
+
+;; Función que recibe una lista de números y regresa una nueva lista que contiene únicamente aquellos
+;; que son felices.
+(define (felices lista)
+    #| Aquí va su código. |#)
+
+;; Función que encuentra el factorión de un número.
+;; factorionr: number -> number
+(define (factorionr n)
+    #| Aquí va su código. |#)
+
+;; Función que encuentra el factorión de un número.
+;; factorionl: number -> number
+(define (factorionl n)
+    #| Aquí va su código. |#)
